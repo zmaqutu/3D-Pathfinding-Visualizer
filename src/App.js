a3a697bb4efbfab35cabf39ebd2ff90270b9369b
Zongo Maqutu <MQTZON001@myuct.ac.za>
2020-10-30 14:48:50 +0200

    Added a grid instead of plane. Will fix bird eye view & implement Dijkstra

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Grid from './components/Grid';
 import { Canvas } from "react-three-fiber";
 import Floor from './components/Floor';
 
-import { Plane, OrbitControls } from 'drei'
+import { Plane, OrbitControls, PerspectiveCamera } from 'drei'
 
 
 function App() {
+  let width = window.innerWidth,
+				height = window.innerHeight;
   return (
     <>
-      <Canvas colorManagement camera={{position: [-20, 30, 5], fov: 60}}>
+      <Canvas colorManagement camera={{position: [-20, 30, 0], fov: 50}}>
         <ambientLight intensity = {0.3}/>
-        <Floor>
-        </Floor>
+        <Floor />
         <Grid />
-        {/*<Plane attach= "material" color="hotpink" />*/}
         <OrbitControls />
       </Canvas>
     
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,11 +4,13 @@ import {useFrame } from "react-three-fiber";
 
 function Grid() {
     const mesh = useRef(null);
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.005))
+    {/*useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.005));*/}
     return (
         <mesh ref = {mesh} position = {[0,2,-5]}>
-          <boxBufferGeometry attach = 'geometry' args = {[5, 5,0]} />
-          <meshStandardMaterial attach = 'material' color="hotpink"/> 
+          {/*<boxBufferGeometry attach = 'geometry' args = {[5, 5,0]} />
+          <meshStandardMaterial attach = 'material' color="hotpink" />*/}
+          <gridHelper args = {[30, 30, "hotpink", "hotpink"] }/>
+          <axesHelper />
         </mesh>
     )
 }
