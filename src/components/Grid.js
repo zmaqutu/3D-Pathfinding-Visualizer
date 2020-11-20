c17ababf58c8d85d9fbce246d0aa000218a9e0ee
Zongo Maqutu <MQTZON001@myuct.ac.za>
2020-11-21 00:26:32 +0200

    Successfuly colored start and finish nodes. Merge back with Main

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,87 +5,141 @@ import Tiles from './Tiles';
 import { Component } from 'react'
 import { Canvas } from "react-three-fiber";
 import img from './floor_texture.jpg';
+import { tweenToColor } from './algorithms/helpers'
+import TWEEN, { Tween } from '@tweenjs/tween.js';
 
- let arr = new Array(1);
-
-function initializeGrid(){
-  let tempGrid = [];
-  for(let i = 0; i < 30; i++){
-      let currentRow = [];
-      for(let j = 0; j < 30; j++){
-          let node = createNode(i, j);
-          currentRow.push(node);
-      }
-      tempGrid.push(currentRow);
-  }
-  console.log(tempGrid);
-
-  for(let i = 0; i < 1; i++){
-      arr[i] = 0;
-  }
- return tempGrid;
-
-}
-
-function createNode(row, col){
-
-  let status = "status";
-  let faces = {};
-  let faceIndex = row * 2 * 30 + col * 2 ;
-  console.log("FaceIndex is: " + faceIndex);
-  {/*faces[1] = this.ground.geometry.faces[faceIndex];
-	faceIndex = faceIndex % 2 == 0 ? faceIndex + 1 : faceIndex - 1;
-	faces[2] = this.ground.geometry.faces[faceIndex];*/}
-  let node = {
-      id: row * col + col,
-      row: row,
-      col: col,
-      faces: faces,
-      status: status,
-      distance: Infinity,
-      totalDistance: Infinity,
-      heuristicDistance: null,
-      direction: null,
-      weight: 0,
-      previousNode: null,
-  };
-
-  return node;
-}
 
 function Grid(props) {
+
+  let groundGeometry = props.worldProperties.groundGeometry;
+  let groundMaterial;
+  let groundMesh;
+  useEffect(() => {
+    // code to run on component mount
+
+  }, []);
+
+  const loader = useMemo(() => new THREE.TextureLoader().load(img,
+    function(texture){
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.x = 30;
+        texture.repeat.y = 30;
+        groundMaterial = new THREE.MeshLambertMaterial({
+          map: texture,
+          side: THREE.FrontSide,
+          vertexColors: THREE.FaceColors,
+        });
+        groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+				groundMesh.receiveShadow = true;
+    }), [img]);
+
     const mesh = useRef(null);
     {/*Declare and initialize state variables/ */}
-    const [grid, setGrid] = useState([]);
+    const [terrain, setTerrain] = useState({
+      grid: initializeGrid(),
+    });
+  
+  function initializeGrid(){
+    let tempGrid = [];
+    for(let i = 0; i < 30; i++){
+        let currentRow = [];
+        for(let j = 0; j < 30; j++){
+            let node = createNode(i, j);
+            currentRow.push(node);
+        }
+        tempGrid.push(currentRow);
+    }
+    renderLoop();
+   return tempGrid;
+  }   
 
-    useEffect(() => {
-      // code to run on component mount
-      setGrid(initializeGrid());
-    }, []);
+  function createNode(row, col){
 
-    const loader = useMemo(() => new THREE.TextureLoader().load(img,
-      function(texture){
-          texture.wrapS = THREE.RepeatWrapping;
-          texture.wrapT = THREE.RepeatWrapping;
-          texture.repeat.x = 30;
-          texture.repeat.y = 30;
-      }), [img]);
+    let status = "default";
+    let faces = {};
+    let faceIndex = row * 2 * props.worldProperties.cols + col * 2 ;
+    faces[1] = groundGeometry.faces[faceIndex];
+    faceIndex = faceIndex % 2 === 0 ? faceIndex + 1 : faceIndex - 1;
+    faces[2] = groundGeometry.faces[faceIndex];
+
+    if(row === props.worldProperties.start.row && col === props.worldProperties.start.col){
+      status = "start";
+    }
+    else if(row === props.worldProperties.finish.row && col === props.worldProperties.finish.col){
+      status = "finish";
+    }
+    let node = {
+        id: row * col + col,
+        row: row,
+        col: col,
+        faces: faces,
+        status: status,
+        distance: Infinity,
+        totalDistance: Infinity,
+        heuristicDistance: null,
+        direction: null,
+        weight: 0,
+        previousNode: null,
+    };
+    
+    if(status === "start"){
+      console.log(node.faces[1])
+      tweenToColor(node, groundGeometry, [props.worldProperties.colors.start]);
       
+    }
+    else if(status === "finish"){
+      tweenToColor(node, groundGeometry, [props.worldProperties.colors.finish]);
+    }
+    return node;
+  }
+  function renderLoop(){
+    window.requestAnimationFrame(renderLoop);
+    TWEEN.update();
+  }
+
+  {/*groundGeometry.rotateY(-Math.PI / 4);
+
+  let loader1 = new THREE.TextureLoader();
+			let floortiles = loader1.load(img,
+				function(texture) {
+					texture.wrapS = THREE.RepeatWrapping;
+					texture.wrapT = THREE.RepeatWrapping;
+					texture.repeat.x = 30;
+					texture.repeat.y = 30;
+				},
+				undefined,
+				function(error) {
+					console.log(error);
+				}
+			);*/}
+  
+  //groundMaterial = THREE.MeshLambertMaterial({map: loader});
+  //const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial)
+
+  
+  groundMaterial = new THREE.MeshLambertMaterial({
+    map: loader,
+    side: THREE.FrontSide,
+    vertexColors: THREE.FaceColors,
+  });
+  console.log(groundMaterial);
+     
     return (
         <mesh ref = {mesh} position = {[0,0,0]}>
           <gridHelper args = {[300, props.gridDimensions, 0x5c78bd, 0x5c78bd] }/>
           <mesh rotation={[-Math.PI /2, 0, 0]} position={[0,-0.1,0]} receiveShadow = {true}>
-            <planeBufferGeometry attach = "geometry" args = {[300,300]}  />
-            <meshLambertMaterial attach = 'material' transparent>
-                <primitive attach="map" object={loader} />
-            </meshLambertMaterial>
-        </mesh>
+            <primitive attach = 'geometry' object = {groundGeometry}  />  
+            <primitive attach = 'material' object = {groundMaterial}  />     
+            
+          </mesh>
+
         {/*<mesh rotation={[-Math.PI /2, 0, 0]} position={[0,-0.09,0]}>
           <planeBufferGeometry attach = "geometry" args = {[300,300]}/>
           <meshLambertMaterial attach = "material" color = "black" side={THREE.FrontSide} opacity= {0.5}/>
-        </mesh>*/}
+    </mesh>*/}
           <axesHelper />
-        </mesh>
+    </mesh>
     )
 }
 
--- src/components/World.js
+++ src/components/World.js
@@ -5,61 +5,57 @@ import { Canvas } from "react-three-fiber";
 import Floor from './Floor';
 import Tiles from './Tiles';
 import { Plane, OrbitControls, PerspectiveCamera } from 'drei';
-import { Scene } from 'three';
-
-{/*function initializeGrid(){
-    for(let i = 0; i < this.rows; i++){
-        let currentRow = [];
-        for(let j = cols; j < this.cols; j++){
-            let node = this.createNode(i, j);
-            currentRow.push(node);
-        }
-    }
-
-}
-
-function createNode(row, col){
-    let faces = {};
-    let faceIndex = row * 2 * this.cols + col * 2;
-
-    let node = {
-        id: row * this.cols + col,
-        row: row,
-        col: col,
-        faces: faces,
-        status: status,
-        distance: Infinity,
-        totalDistance: Infinity,
-        heuristicDistance: null,
-        direction: null,
-        weight: 0,
-        previousNode: null,
-    };
-
-    if (status == "start") {
-        tweenToColor(node, this.ground.geometry, [this.colors.start]);
-    } else if (status == "finish") {
-        tweenToColor(node, this.ground.geometry, [this.colors.finish]);
-    }
-
-    return node;
-}*/}
+import { PlaneBufferGeometry, Scene } from 'three';
 
 function World(props) {
-   let width = window.innerWidth/2;
-   let height = window.innerHeight/2;
+   let width = window.innerWidth;
+   let height = window.innerHeigh;
+
+   const groundGeometry1 = new THREE.PlaneGeometry(300,300,30,30);
+   const groundMaterial1 = new THREE.MeshLambertMaterial({transparent: false, color: "hotpink"});
+
+   const groundMesh = new THREE.Mesh(groundGeometry1, groundMaterial1);
+
+   let renderer = new THREE.WebGL1Renderer({ antialias: true });
+   renderer.setSize(width, height);
+   renderer.shadowMap.enabled = true;
 
     return (
         <>
-        <Canvas colorManagement camera={{position: [0, 350, 0], fov: 50, aspect: width / height, far: 5000}} gl={{antialias: true}} raycaster= {new THREE.Raycaster()}>
+        <Canvas colorManagement camera={{position: [0, 350, 0], fov: 50, aspect: width / height, far: 5000}} gl={renderer} raycaster= {new THREE.Raycaster()}>
         <ambientLight intensity = {1} color = {0xBBC2D0}/>
         {/*<fog attach = "fog" args = {[0xFFFFFF , 0, 750]}/>*/}
         <hemisphereLight color = {"hsl(0.6, 1, 0.6)"} groundColor = {0x87775d} intensity = {0.1} position = {[0,5,0]} />
-        <directionalLight color = {"hsl(0.1, 1, 0.95)"} groundColor = {0x87775d} intensity = {0.5} position = {[-70,122.5,70]} castShadow = {true}>
-            
+        <directionalLight color = {"hsl(0.1, 1, 0.95)"} groundColor = {0x87775d} intensity = {0.5} position = {[-70,122.5,70]} castShadow = {true}>     
         </directionalLight>
         <Floor/>
-        <Grid gridDimensions = {30}/>
+        <Grid gridDimensions = {30} worldProperties = {
+            {
+                rows: 30,
+                cols: 30,
+                groundGeometry : new THREE.PlaneGeometry(300,300,30,30),
+                groundMaterial : new THREE.MeshLambertMaterial({transparent: true, color: "hotpink"}),
+                start: {
+                    row: 3,
+                    col: 5,
+                },
+                finish: {
+                    row: 16,
+                    col: 22,
+                },
+                colors: {
+                    start: {r: 0, g: 1, b: 0 },
+                    finish: {r: 1, g: 0, b: 0},
+                    wall: {r: 0.109, g: 0.109, b: 0.45},
+                    visited: {r: 0.329, g: 0.27, b: 0.968},
+                    path: {r: 1, g: 1, b: 0},
+                },
+                nodeDimensions:{
+                    height: 10,
+                    width: 10,
+                },
+            }
+        }/>
         <OrbitControls />
       </Canvas>
       </>
